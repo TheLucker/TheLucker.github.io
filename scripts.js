498c47259b22df8dfc46d4e4dafbbb7e479561ab
TheLucker <69870245+TheLucker@users.noreply.github.com>
2021-07-27 04:11:28 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* jQuery thingy */
+var script = document.createElement('script'); 
+ 
+script.src = '//code.jquery.com/jquery-1.11.0.min.js'; 
+document.getElementsByTagName('head')[0].appendChild(script);  
+
+        <div class="navbar">
+            <a href="#" class="active">Home </a>
+            <a href="#">Work in Progress</a>
+            <a href="downloads.html">Downloads</a>
+            <a href="https://discord.gg/kVEmqNcZ6Q" class="right">Discord</a>
+            <a href="/database/tools/" class="right">Tools</a>
+        </div>
